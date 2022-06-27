b4e262524a34ff0ebab0d92f4ebad4a0ca0610ad
Thien K. M. Bui <kevinbui904@gmail.com>
2022-06-27 18:13:59 +0100

    fixed up Problem2

--- ps01/Problem2.py
+++ ps01/Problem2.py
@@ -5,86 +5,7 @@
 # David Liben-Nowell (dln@carleton.edu)
 # Adela Dujsikova and Kevin Bui
 
-import sys
-from collections import defaultdict
-
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-def ltr2int(ch):
-    '''Translate letters (upper or lower case) to indices {0, 1, ..., 25}'''
-    return ord(ch.upper()) - ord('A')
-
-def int2ltr(i):
-    '''Translate indices {0, 1, ..., 25, 26=0, 27=1, ...} to (upper case)
-       letters, wrapping around as necessary.'''
-    return chr(i % 26 + ord('a'))
-
-def load_file(filename):
-    '''Load all of the text from the given file into a single string.'''
-    with open(filename, "r") as f:
-        s = "".join(line.rstrip() for line in f)
-    return s.split(" ")
-
-
-
-
-def count_Ngram_frequency(text, n=1):
-    '''Count all sequences of N consecutive letters in the given string,
-       and return a (default) dictionary of those counts.  Does NOT
-       maintain case; all counts are for the input string converted to
-       all upper case.  (So n=1 is unigrams, n=2 is bigrams, etc.)'''
-    counts = defaultdict(int)
-    for i in range(len(text) - n + 1):
-        ngram = text[i:i+n]
-        if n == 1 and len(ngram[0])>0:
-            counts[int("".join(ngram))] += 1
-        elif len(ngram[0])>0:
-            counts[tuple(ngram)] += 1
-    return counts
-    # return {key: v/(len(text)-1) for key, v in counts.items()}
-
 def breakHomophonic(c):
-
-    ciphertext = load_file(c)
-    unigrams = count_Ngram_frequency(ciphertext, 1)
-    bigrams = count_Ngram_frequency(ciphertext, 2)
-    trigrams = count_Ngram_frequency(ciphertext, 3)
-    quadgram = count_Ngram_frequency(ciphertext, 4)
-
-    unigrams_sorted = dict(sorted(unigrams.items(),key= lambda x:x[1]))
-    bigrams_sorted = dict(sorted(bigrams.items(),key= lambda x:x[1]))
-    trigrams_sorted = dict(sorted(trigrams.items(), key = lambda x:x[1]))
-    quadgrams_sorted = dict(sorted(quadgram.items(), key = lambda x:x[1]))
-
-    # print(unigrams_sorted)
-
-    # for key in quadgrams_sorted.keys():
-    #     if key[0] == "6":
-    #         print(key, quadgrams_sorted[key])
-    # for key in bigrams_sorted.keys():
-    #     if bigrams_sorted[key] > 100:
-    #         print(key)
-    
-    # for key in trigrams_sorted.keys():
-    #     if trigrams_sorted[key] > 20:
-    #         print(key, trigrams_sorted[key])
-    # print(quadgram_sorted)
-
-#cipher table
-#   t   |   94 
-#   h   |   5
-#   e   |   90, 69, 36, 53, 23, 64, 94
-
-#   q   |   74
-#   z   |   59
-#   u   |   60, 88, 32
-#   i   |   94, 64, 23, 69, 53, 36
-#   n   |   30, 35, 5, 15, 41, 11
-#   g   |   14, 90
-#   y   |   92, 77
-#   o   |   1, 7, 51, 83, 24, 13, 89, 9
-
     cipher_map = {
         "0": "t",
         "1": "o",
@@ -188,6 +109,7 @@ def breakHomophonic(c):
         "99": "f",
     }
 
+    new_file = open("decrypted_homophonic.txt", "w")
 
     with open("homophonic_substitution.txt", "r") as f:
         for line in f:
@@ -198,12 +120,11 @@ def breakHomophonic(c):
                     new_string += cipher_map[word]
                 else:
                     new_string += " "+ word + " "
+            new_file.writelines(new_string + "\n")
             print(new_string)
+    
+    new_file.close()
         
     return None
 
 breakHomophonic('homophonic_substitution.txt')
-
-
-# z, q, j, x 
-# 59 74 10 6
