322d30ee3c5ad5086d0cda02c74f1658d9ed44d1
Thien K. M. Bui <kevinbui904@gmail.com>
2022-07-04 18:30:33 +0100

    finished question 1

--- ps02/enigma.py
+++ ps02/enigma.py
@@ -5,7 +5,7 @@
 # David Liben-Nowell (dln@carleton.edu)
 
 import argparse
-from substitution_ciphers import ltr2int, int2ltr, ALPHABE
+from substitution_ciphers import ltr2int, int2ltr, ALPHABET
 
 # Details on the rotors as listed here are from a variety of online
 # sources.  Rotor details come from Tony Sale's Codes and Ciphers.
