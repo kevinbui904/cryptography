6403871d16f2906c5d6e2760cf94c67fd39e8128
Thien K. M. Bui <kevinbui904@gmail.com>
2022-07-08 00:02:04 +0100

    finished problem 4

--- ps02/signature.py
+++ ps02/signature.py
@@ -33,6 +33,9 @@ KEYS = [
     "ZZZZZZ"
 ]
 
+ALPHABET = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q'
+            ,'R','S','T','U','V','W','X','Y','Z']
+
 #build_graph()
 # @param lst , list of messages
 # @return graph, dictionary graph containing all links from each encryption
@@ -44,30 +47,25 @@ def build_graph(lst):
         graph[message[0]] = message[3]
     return graph
 
-#signature()
-# @param lst, dictionary of nodes
-# @return list of ints detailing the length of each nodes cycle
-
 def signature(lst):
     node_graph = lst
     signature = []
 
-    keys = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q'
-            ,'R','S','T','U','V','W','X','Y','Z']
 
     cycle = []
     
-    for key in keys:
-        if key in node_graph.keys():
-            while key in node_graph.keys():
-                cycle.append(key)
-                value = node_graph[key]
-                del node_graph[key]
-                key = value
+    for letter in ALPHABET:
+        if letter in node_graph.keys():
+            while letter in node_graph.keys():
+                cycle.append(letter)
+                value = node_graph[letter]
+                del node_graph[letter]
+                letter = value
             signature.append(len(cycle))
             cycle = []
     
     return signature
+
 
 #calc
 def main():
@@ -76,7 +74,10 @@ def main():
         "Specify rotors from left to right (slow to fast).\n" + \
         "E.g. III is the slow rotor in [python enigma.py ABCDEF III II I ADO]"
 
+
+    config = ['m', 'c']
     parser = argparse.ArgumentParser(description=helptext)
+    parser.add_argument('config', help="m for manual testing, c for problem 4. -c will ignore all subsequent inputs", choices=config)
     parser.add_argument('slow_rotor', help="slow [left] rotor", choices=rotors)
     parser.add_argument('medium_rotor', help="middle rotor", choices=rotors)
     parser.add_argument('fast_rotor', help="fast [right] rotor", choices=rotors)
@@ -86,18 +87,56 @@ def main():
     if len(set([args.slow_rotor,args.medium_rotor,args.fast_rotor])) != 3:
         raise ValueError("You cannot reuse a rotor.")
         
-    machine = enigma(args.slow_rotor, args.medium_rotor, args.fast_rotor,
-                     [], "AAA", args.initial_rotor_settings)
-    
-    messages = []
+    if args.config == 'm':
 
-    #have to reset the machine after every encryption
-    for key in KEYS:
-        messages.append(machine(key))
         machine = enigma(args.slow_rotor, args.medium_rotor, args.fast_rotor,
-                     [], "AAA", args.initial_rotor_settings)
-    node_graph = build_graph(messages)
-    print(signature(node_graph))
+                        [], "AAA", args.initial_rotor_settings)
+        
+        messages = []
+
+        #have to reset the machine after every encryption
+        for key in KEYS:
+            messages.append(machine(key))
+            machine = enigma(args.slow_rotor, args.medium_rotor, args.fast_rotor,
+                        [], "AAA", args.initial_rotor_settings)                    
+        node_graph = build_graph(messages)
+        print(signature(node_graph))
+    
+    else:    
+        #write out the signature for all configuration into a new file
+        #every triplet of rotors and every initial setting (5*4*3*26^3 signatures)
+        file = open("signatures.txt", "w")
+        
+        count = 0
+        #rotors
+        for slow in rotors:
+            for med in rotors:
+                if med != slow:
+                    for fast in rotors:
+                        if fast != slow and fast != med:
+                            for first_ltr in ALPHABET:
+                                for second_ltr in ALPHABET:
+                                    for third_ltr in ALPHABET:
+                                        if count < 10000:
+                                            count += 1
+                                            messages = []
+
+                                            machine = enigma(slow, med, fast,
+                                                            [], "AAA", first_ltr+second_ltr+third_ltr)  
+                                            #have to reset the machine  after every encryption
+                                            for key in KEYS:
+                                                messages.append(machine(key))
+                                                machine = enigma(slow, med, fast,
+                                                            [], "AAA", first_ltr+second_ltr+third_ltr)                    
+                                            node_graph = build_graph(messages)
+                                            sig = signature(node_graph)
+                                            txt = slow + " " + med + " " + fast + " " + "\""+ first_ltr+second_ltr+third_ltr+"\"" + "\n" + str(sig) + "\n"
+                                            file.write(txt)
+                                        else:
+                                            file.close()
+                                            exit(1)
+
+                                          
     
 
 if __name__ == "__main__":
